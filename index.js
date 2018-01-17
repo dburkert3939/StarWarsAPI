b6b7922890db06a5c75461942f9dcf1fa32b611a
Dustin Burkert <dustinburkert5@gmail.com>
2018-01-17 16:58:28 -0500

    added ajax get request

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 $(() => {
   
+ $('#infoButton').click(() => {
+     $.ajax({
+        type: 'GET',
+        url: 'https://swapi.co/api/people'
+     }).done((res) => {
+         let people = res.results
+         for (person of people) {
+             console.log(person.name)
+         }
+     })
+ })
 })
 
